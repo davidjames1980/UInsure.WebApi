146e0a6e4456faaba207ac7f060f3e7889f78f11
DjfSoftware <daveforbes@autocodingsystems.com>
2026-02-22 14:30:24 +0000

    Adding the full .http suite

--- UInsure.WebApi.DavidJames.Models/RefundModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UInsure.WebApi.DavidJames.Models
-{
-    public class RefundModel
-    {
-        public required string RefundReference { get; set; }
-        public required string Type { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
